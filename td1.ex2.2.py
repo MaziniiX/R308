c4cf64dd4df52e4acf2ecbd56a523fdf97aca5fa
MaziniiX <bluenderbot@gmail.com>
2023-09-13 16:48:15 +0200

    a faire: le del et les autres exos

--- td1.ex2.2.py
+++ td1.ex2.2.py
@@ -1,2 +1,23 @@
 class Tasse:
-    matiere:str="ceramique"
+    matiere:str="céramique"
+    def __init__(self, couleur:str, contenance:int, marque:str):
+        self.contenance = contenance
+        self.marque = marque
+        self.couleur = couleur
+
+    def __str__(self):
+        return f"la tasse de matière {Tasse.matiere}, de couleur {self.couleur} et de marque {self.marque} a une contenance de {self.contenance} ml"
+
+    def contenu(self, boisson:str):
+        self.boisson = boisson
+        return f"et contient du {boisson}"
+
+    def boire_tasse(self):
+        del(self)
+        
+        
+
+
+t=Tasse("bleu",50,"ikea")
+b=t.contenu("café")
+print(t,b)
